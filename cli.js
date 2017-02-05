7e1967d90db125a5bca7e0496bc018ad5bd46c99
Vadim Demedes <vdemedes@gmail.com>
2017-02-05 23:01:29 +0200

    add update notifier

--- cli.js
+++ cli.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 const {spawn} = require('child_process');
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+
+updateNotifier({pkg}).notify();
 
 const args = process.argv.slice(2);
 spawn('npm', ['start', '--'].concat(args), {
